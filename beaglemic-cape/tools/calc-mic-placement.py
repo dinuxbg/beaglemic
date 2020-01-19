2dc7ad5501d8d6c3a79297102b905a27fe213b42
Dimitar Dimitrov <dimitar@dinux.eu>
2020-01-19 13:32:39 +0200

    beaglemic-cape: Shrink PCB to 100x100mm
    Let's keep the PCB in the hobby price range.
    
    Version 1.0-rc2
    
    TODO: Route me!
    
    Signed-off-by: Dimitar Dimitrov <dimitar@dinux.eu>

---
+++ beaglemic-cape/tools/calc-mic-placement.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# Calculate placement coordinates of microphones.
+
+from numpy import exp, abs, angle, pi
+
+# Center of our board.
+CENTER = 133.3 + 1j * 94.075
+R = 46
+
+def polar2z(r,theta):
+    return r * exp( 1j * theta )
+
+def z2polar(z):
+    return ( abs(z), angle(z) )
+
+
+def main():
+    for i in range(16):
+        r,a = z2polar(R)
+        # KiCad has Y growing downwards, so revert rotation.
+        a -= i * 2 * pi / 16
+        p = polar2z(r,a) + CENTER
+        a_deg = (a * 180) / pi
+        print("mic%02d: rot %f: %f x %f" % (i, -a_deg, p.real, p.imag))
+
+if __name__ == "__main__":
+    main()
