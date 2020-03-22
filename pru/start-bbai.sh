fdeda89d685ca707740876c4847f6e738dcb4567
Dimitar Dimitrov <dimitar@dinux.eu>
2020-03-22 17:17:07 +0200

    Add missing shell script for BBAI
    Setup differs between PocketBeagle and BBAI:
      - PinMux can only be done from DTS on BBAI.
      - Remoteproc numbers are different.
    
    Signed-off-by: Dimitar Dimitrov <dimitar@dinux.eu>

---
+++ pru/start-bbai.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+RPROC0=/sys/class/remoteproc/remoteproc2
+RPROC1=/sys/class/remoteproc/remoteproc3
+
+echo stop > $RPROC0/state
+echo stop > $RPROC1/state
+
+# PinMux must be setup from DeviceTree, so go ahead
+# directly to remoteproc initialization.
+
+cp out/pru-core0.elf /lib/firmware/
+cp out/pru-core1.elf /lib/firmware/
+echo pru-core0.elf > $RPROC0/firmware
+echo pru-core1.elf > $RPROC1/firmware
+
+echo start > $RPROC0/state
+echo start > $RPROC1/state
