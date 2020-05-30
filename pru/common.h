cbe2f68b834d2adb81adac9925f43193c9f650c7
Dimitar Dimitrov <dimitar@dinux.eu>
2020-05-30 20:10:55 +0300

    PRU0: Reset microphone when switching modes
    This fixes occasional increased noise in some channels
    when switching between 16ch and 8ch modes.
    
    Signed-off-by: Dimitar Dimitrov <dimitar@dinux.eu>

--- pru/common.h
+++ pru/common.h
@@ -21,7 +21,13 @@
 #define SCRATCH_BANK_2	12
 #define PEER_PRU_BANK	14
 
-#define REG_PRU0_MODE	r29.b0
+#define REG_PRU0_MODE		r29.b0
+#define INTC_COMPOSITE_SYS_BASE	16
+#define PRU0_TO_PRU1_INTR	(21 - INTC_COMPOSITE_SYS_BASE)
+#define R31_INTR_STROBE		(1u << 5)
+
+#define R30_CLK_PIN		14
+
 
 #define CMD_START	0x01
 #define CMD_STOP	0x02
